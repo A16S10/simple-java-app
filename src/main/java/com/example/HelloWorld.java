2a1063430d008abb6288e925b9339ebbbdb3325b
arshad <arshad@Ubuntu.myguest.virtualbox.org>
2024-08-11 22:53:49 +0530

    new chnages

--- src/main/java/com/example/HelloWorld.java
+++ src/main/java/com/example/HelloWorld.java
@@ -1,9 +1,11 @@
-// HelloWorld.java
+package com.example;
+
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("This is simple java file");
     }
 }
+
 
 
 
